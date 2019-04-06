3bddeecfda716a0a6268f2c56c55f4f0faffffe5
Pavlo Zhukov <pahazhukov@gmail.com>
2019-04-06 03:39:08 +0300

    Configure src components resolving via node_modules

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -248,9 +248,10 @@ module.exports = function(webpackEnv) {
       // We placed these paths second because we want `node_modules` to "win"
       // if there are any conflicts. This matches Node resolution mechanism.
       // https://github.com/facebook/create-react-app/issues/253
-      modules: ['node_modules'].concat(
+      modules: [paths.appNodeModules].concat(
         // It is guaranteed to exist because we tweak it in `env.js`
-        process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+        process.env.NODE_PATH.split(path.delimiter).filter(Boolean),
+        paths.appSrc
       ),
       // These are the reasonable defaults supported by the Node ecosystem.
       // We also include JSX as a common component filename extension to support
